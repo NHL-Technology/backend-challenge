a8582470603a83fa8ab90bbe3592e8f2ed478687
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-12-10 17:53:34 -0500

    refactor: remove unused console commands, update web routes for API usage, and delete Vite configuration file.

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,4 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+// No se utilizan comandos custom en este proyecto
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Este proyecto solo usa rutas API
+// Ver: routes/api.php
